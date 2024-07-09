aa235f665cc1e7bd4c3bb04a91531417cf223ac5
Moaz Mohamed <131017117+TherealZoux@users.noreply.github.com>
2024-07-10 02:38:30 +0300

    Add files via upload

---
+++ dist/assets/favoriteView-BTxTbN4f.js
@@ -0,0 +1,1 @@
+import{_ as e,l as t,o}from"./index-DxQWsM2D.js";const c={},n={class:"m-auto text-center text-4xl text-[#A1C398]"};function s(r,a){return o(),t("h1",n,"coming soon")}const f=e(c,[["render",s]]);export{f as default};

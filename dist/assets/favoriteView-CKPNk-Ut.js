3a83c89faf4a3ff306b57e909486a11a61623059
therealzoux <itszoux@gmail.com>
2024-08-06 12:03:16 +0300

    wiki-cast 0.2

---
+++ dist/assets/favoriteView-CKPNk-Ut.js
@@ -0,0 +1,1 @@
+import{_ as e,k as t,o,f as s}from"./index-DG2ww9j4.js";const c={},n={class:"mt-[4rem] p-20"},a=s("h1",{class:"m-auto text-center text-4xl text-[#fff]"},"coming soon",-1),r=[a];function _(f,i){return o(),t("section",n,r)}const d=e(c,[["render",_]]);export{d as default};
